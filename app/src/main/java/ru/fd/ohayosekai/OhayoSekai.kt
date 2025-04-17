b392214fdca5d303cc298883b338d95e0fe48731
FastestDan <75726665+FastestDan@users.noreply.github.com>
2025-04-17 21:33:36 +1000

    The app

---
+++ app/src/main/java/ru/fd/ohayosekai/OhayoSekai.kt
@@ -0,0 +1,15 @@
+package ru.fd.ohayosekai
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class OhayoSekai: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_ohayo_sekai)
+    }
+
+}
