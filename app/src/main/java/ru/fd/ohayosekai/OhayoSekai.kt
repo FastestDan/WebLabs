d7f3f934ad994abda5633028b3cd4353b6cd4119
FastestDan <75726665+FastestDan@users.noreply.github.com>
2025-04-23 01:23:17 +1000

    Added dummy function
    Still don't know how to do fancy-schmancy text

--- app/src/main/java/ru/fd/ohayosekai/OhayoSekai.kt
+++ app/src/main/java/ru/fd/ohayosekai/OhayoSekai.kt
@@ -3,13 +3,22 @@ package ru.fd.ohayosekai
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.text.SpannableString
+import android.text.style.ClickableSpan
+
+import android.view.View
 
 class OhayoSekai: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_ohayo_sekai)
+        setContentView(R.layout.welcome)
     }
 
+}
+class MySpan: ClickableSpan(){
+    override fun onClick(p0: View) {
+        TODO("Not yet implemented")
+    }
 }
